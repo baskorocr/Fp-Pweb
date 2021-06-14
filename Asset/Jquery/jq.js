11a68c1a8177e9cedc240f670b2162898ee48e8b
baskorocr <61825549+baskorocr@users.noreply.github.com>
2021-06-14 19:29:22 +0700

    Delete Asset directory

--- Asset/Jquery/jq.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-<<<<<<< HEAD
-  $('.content').load('nav.html');
-=======
-  $('#nav').load('nav.html');
-  $('#footer').load('footer.html');
->>>>>>> de92c2f70c26f67ef43abf33c6060b395ca0d898
-});
