fa90c2eaeccf6cff95d72e2a1768f5b0536922f4
baskorocr <mahesa.23@students.amikom.ac.id>
2021-06-27 14:10:34 +0700

    no message

--- Asset/JS/Keranjang.js
+++ Asset/JS/Keranjang.js
@@ -28,33 +28,35 @@ function ready() {
       var keranjang = firebase.database().ref('keranjang/' + user.uid + '/');
       keranjang.on('value', (snap) => {
         var json = snap.val();
-        var idKeranjang = Object.keys(json);
-        var dataKeranjang = Object.values(json);
-        var item = document.getElementById('item');
+        if (json != null) {
+          var idKeranjang = Object.keys(json);
+          var dataKeranjang = Object.values(json);
+          var item = document.getElementById('item');
 
-        for (let y = 0; y < idKeranjang.length; y++) {
-          item.innerHTML +=
-            '<div class="row " id="t"> <div class="col-4"><img src="asset/Img/download.png" alt="" style="height: 100px;" class="img"></div> <div class="col"><div class="row "> <div class="col "><p class="namabarang">dsad</p> <p class="hargabarang">dadsa</p></div> </div></div> </div>';
-        }
+          for (let y = 0; y < idKeranjang.length; y++) {
+            item.innerHTML +=
+              '<div class="row " id="t"> <div class="col-4"><img src="asset/Img/download.png" alt="" style="height: 100px;" class="img"></div> <div class="col"><div class="row "> <div class="col "><p class="namabarang">dsad</p> <p class="hargabarang">dadsa</p></div> </div></div> </div>';
+          }
 
-        var getImg = document.getElementsByClassName('img');
-        var getNama = document.getElementsByClassName('namabarang');
-        var getHarga = document.getElementsByClassName('hargabarang');
-        for (var i = 0; i < getImg.length; i += 1) {
-          getImg[i].src = dataKeranjang[i]['link'];
-          getNama[i].innerHTML = dataKeranjang[i]['Pesanan'];
-          getHarga[i].innerHTML = dataKeranjang[i]['harga'];
-        }
+          var getImg = document.getElementsByClassName('img');
+          var getNama = document.getElementsByClassName('namabarang');
+          var getHarga = document.getElementsByClassName('hargabarang');
+          for (var i = 0; i < getImg.length; i += 1) {
+            getImg[i].src = dataKeranjang[i]['link'];
+            getNama[i].innerHTML = dataKeranjang[i]['Pesanan'];
+            getHarga[i].innerHTML = dataKeranjang[i]['harga'];
+          }
 
-        document.getElementById('jml').innerHTML =
-          idKeranjang.length + ' ' + 'Produk';
-        let total = 0;
-        for (let p = 0; p < idKeranjang.length; p++) {
-          var temp = parseInt(dataKeranjang[p]['harga']);
-          total = total + temp;
+          document.getElementById('jml').innerHTML =
+            idKeranjang.length + ' ' + 'Produk';
+          let total = 0;
+          for (let p = 0; p < idKeranjang.length; p++) {
+            var temp = parseInt(dataKeranjang[p]['harga']);
+            total = total + temp;
+          }
+          document.getElementById('gratis').innerHTML = 'Gratis';
+          document.getElementById('total').innerHTML = 'Rp. ' + total;
         }
-        document.getElementById('gratis').innerHTML = 'Gratis';
-        document.getElementById('total').innerHTML = 'Rp. ' + total;
       });
     } else {
       window.location.href = 'Login.html';
